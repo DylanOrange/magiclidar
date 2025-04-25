a5ec6856af436a32ec4686e8b0a7243851cb70d5
DylanOrange <dongyue.lu@tum.de>
2025-04-25 21:58:58 +0800

    update event branch

--- datasets/torchvision_datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-# ------------------------------------------------------------------------
-# BEAUTY DETR
-# Copyright (c) 2022 Ayush Jain & Nikolaos Gkanatsios
-# Licensed under CC-BY-NC [see LICENSE for details]
-# ------------------------------------------------------------------------
-
-from .coco import CocoDetection
--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Open the JSON file for reading
-with open('OpenSource/final_flickr_mergedGT_train.json', 'r') as file:
-    data = json.load(file)
-
-# Now you can work with the parsed data (usually a dictionary or a list)
-print(data)
